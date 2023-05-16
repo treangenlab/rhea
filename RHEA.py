fb9ddae11e1e85d898d04d9c0a8d2fdded2f9ef6
Kristen Curry <kdc10@rice.edu>
2023-05-16 13:45:32 +0200

    add node length/depth

--- RHEA.py
+++ RHEA.py
@@ -10,7 +10,7 @@ import math
 import logging
 import argparse
 import subprocess
-from collections import defaultdict, Counter
+from collections import defaultdict
 
 import pandas as pd
 import networkx as nx
@@ -37,6 +37,7 @@ def weight_graph(graph_path):
     logging.info("Reading in graph: %s", graph_path)
     graph = nx.Graph()
     sequences = {}
+    nodes_data = []
     with open(graph_path, "r", encoding='UTF-8') as file:
 
         for line in file:
@@ -57,27 +58,31 @@ def weight_graph(graph_path):
 
             if line.startswith("S"):
                 split_line = line[:-1].split("\t")
+                node_depth = int([cov.split(":")[-1] for cov in split_line if cov.startswith("dp")][0])
                 new_node = split_line[1].split("_")[-1]
                 node_length = len(split_line[2])
-                graph.add_node(new_node, weight=node_length)
+                graph.add_node(new_node, size=node_length, weight=node_depth)
                 sequences[new_node] = SeqRecord(Seq(split_line[2]),
                                                 id=new_node,
                                                 description="edge_{}".format(new_node))
-    return graph, sequences
+                nodes_data.append([new_node, node_length, node_depth])
+    nodes_df = pd.DataFrame(nodes_data, columns=['node', 'node_length', 'node_depth'])
+    return graph, sequences, nodes_df
 
 
 def cluster_graph(graph, sequences, genome_avg_len, genome_min_len,
-                  output_path_clusters, output_path_fragments):
+                  nodes_df, output_path_clusters, output_path_fragments):
     """
     Performs spectral clustering on weighted graph, where number of clusters decided such that
     each cluster is the length of one genome.
 
-    :networkx.G graph: weighted assembly graph. Nodes weighted by length of sequence.
+    :networkx.G graph: weighted assembly graph. Nodes size length of sequence and weighted by coverage.
             Edges weighted by coverage between nodes.
     :dict{str:SeqRecord} sequences: dictionary of edge id to SeqRecord sequence
     :int genome_avg_len: expected average genome length for genomes in metagenome sample
     :int genome_min_len: minimum genome length for genomes in metagenome sample. Used as a
             threshold for clusters vs fragments
+    :list of lists nodes_data:
     :str output_path_clusters: path to output clustered fasta files
     :str output_path_fragments: path to output fragments fasta files
     :return: Pandas df mapping graph nodes to assigned clusters
@@ -90,19 +95,22 @@ def cluster_graph(graph, sequences, genome_avg_len, genome_min_len,
     total_bins = 0
     for connected_component in nx.connected_components(graph):
         adj_matrix = nx.to_numpy_array(graph, nodelist=list(connected_component))
-        length_sum = sum([graph.nodes("weight")[node] for node in connected_component])
+        length_sum = sum([int(graph.nodes("size")[node]) for node in connected_component])
         n_clusters = math.ceil(length_sum / genome_avg_len)
-        if n_clusters == 1 or len(connected_component) < 3: # connected component is one cluster
+        if n_clusters == 1 or len(connected_component) < 3:  # connected component is one cluster
             next_labels = [total_bins] * len(connected_component)
-            cluster_seqs = [sequences.pop(node) for node in connected_component]
+            if sequences:
+                cluster_seqs = [sequences.pop(node) for node in connected_component]
             if length_sum < genome_min_len:  # add to fragments list if len_sum under threshold
-                SeqIO.write(cluster_seqs, os.path.join(output_path_fragments, "{}.fna".format(total_bins)),
-                            "fasta")
+                if sequences:
+                    SeqIO.write(cluster_seqs, os.path.join(output_path_fragments, "{}.fna".format(total_bins)),
+                                "fasta")
                 fragment_nodes = fragment_nodes + list(connected_component)
                 fragment_labels = fragment_labels + next_labels
             else:  # else add to clusters
-                SeqIO.write(cluster_seqs, os.path.join(output_path_clusters, "{}.fna".format(total_bins)),
-                            "fasta")
+                if sequences:
+                    SeqIO.write(cluster_seqs, os.path.join(output_path_clusters, "{}.fna".format(total_bins)),
+                                "fasta")
                 all_nodes = all_nodes + list(connected_component)
                 cluster_labels = cluster_labels + next_labels
             cluster_stats[total_bins] = (length_sum, len(connected_component))
@@ -114,26 +122,30 @@ def cluster_graph(graph, sequences, genome_avg_len, genome_min_len,
             for label, component_nodes in zip(next_labels, list(connected_component)):
                 clusters_dict[label].append(component_nodes)
             for k, cluster_nodes in clusters_dict.items():
-                length_cluster = sum([graph.nodes("weight")[node] for node in cluster_nodes])
+                length_cluster = sum([graph.nodes("size")[node] for node in cluster_nodes])
                 cluster_stats[k] = (length_cluster, len(cluster_nodes))
-                cluster_seqs = [sequences.pop(node) for node in cluster_nodes]
-                SeqIO.write(cluster_seqs, os.path.join(output_path_clusters, "{}.fna".format(k)), "fasta")
+                if sequences:
+                    cluster_seqs = [sequences.pop(node) for node in cluster_nodes]
+                    SeqIO.write(cluster_seqs, os.path.join(output_path_clusters, "{}.fna".format(k)), "fasta")
             all_nodes = all_nodes + list(connected_component)
             cluster_labels = cluster_labels + next_labels
         total_bins = total_bins + n_clusters
     clusters_dataframe = pd.DataFrame({"node": all_nodes,
                                        "cluster": cluster_labels})
-    clusters_dataframe["cluster_length"] = clusters_dataframe["cluster"]\
+    clusters_dataframe = clusters_dataframe.merge(nodes_df, on="node", how="left")
+    clusters_dataframe["cluster_length"] = clusters_dataframe["cluster"] \
         .apply(lambda x: cluster_stats[x][0])
-    clusters_dataframe["cluster_size"] = clusters_dataframe["cluster"]\
+    clusters_dataframe["cluster_size"] = clusters_dataframe["cluster"] \
         .apply(lambda x: cluster_stats[x][1])
     fragments_dataframe = pd.DataFrame({"node": fragment_nodes,
                                         "cluster": fragment_labels})
-    fragments_dataframe["fragment_length"] = fragments_dataframe["cluster"]\
+    fragments_dataframe = fragments_dataframe.merge(nodes_df, on="node", how="left")
+    fragments_dataframe["fragment_length"] = fragments_dataframe["cluster"] \
         .apply(lambda x: cluster_stats[x][0])
-    fragments_dataframe["fragment_size"] = fragments_dataframe["cluster"]\
+    fragments_dataframe["fragment_size"] = fragments_dataframe["cluster"] \
         .apply(lambda x: cluster_stats[x][1])
-    return clusters_dataframe, fragments_dataframe
+    return clusters_dataframe.sort_values(['cluster', 'node']), \
+           fragments_dataframe.sort_values(['cluster', 'node'])
 
 
 def classify_clusters(out_dir, clusters_dir, group_type, n_threads):
@@ -175,8 +187,6 @@ def get_viral_bins(table_viral, df_clusters, bin2class_names_path, group_type):
     :str group_type: denote if clusters or fragments
     :return: Pandas df with viral nodes and their cluster information
     """
-    ## clean this up to include node weight of cluster/fragment
-    ## how to handle duplicate rows for the same viral seq
 
     logging.info("Placing viral sequences in {}".format(group_type))
     df_viral = pd.read_csv(table_viral, dtype={"node": "int32"}, sep="\t", index_col="node")
@@ -296,10 +306,17 @@ if __name__ == "__main__":
         if not os.path.exists(output_fragments_dir):
             os.makedirs(output_fragments_dir)
         # create and cluster RHEA graph
-        weighted_graph, seqs = weight_graph(args.input_graph)
+        if os.path.splitext(args.input_graph)[1] == '.graphml':
+            seqs = None
+            df_nodes = pd.DataFrame([], columns=['node'])
+            weighted_graph = nx.read_graphml(args.input_graph)
+
+        else:
+            weighted_graph, seqs, df_nodes = weight_graph(args.input_graph)
         graph_clusters_df, graph_fragments_df = cluster_graph(weighted_graph, seqs,
                                                               args.avg_genome_len,
                                                               args.min_genome_len,
+                                                              df_nodes,
                                                               output_clusters_dir,
                                                               output_fragments_dir)
         graph_clusters_df.to_csv(os.path.join(args.output_dir, "clusters.tsv"), sep='\t', index=False)
